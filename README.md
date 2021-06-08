803c01cac63a7fd4f3998c685bbc5efe5d136985
eramanmalhi <er.amanmalhi@gmail.com>
2021-06-08 19:03:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# education-eureka-server
+ 
---
+++ src/test/java/education/eureka/server/EducationEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package education.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
